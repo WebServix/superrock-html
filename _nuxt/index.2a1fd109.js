ec73ef341795d6bcdfde6df2eb43df978953eea1
Arnaud <thiery.arnaud@gmail.com>
2022-11-28 11:05:48 +0100

    first

---
+++ _nuxt/index.2a1fd109.js
@@ -0,0 +1,1 @@
+import{a as e}from"./entry.2f672c90.js";const n={};function r(c,t){return null}const o=e(n,[["render",r]]);export{o as default};
---
+++ _nuxt/index.ed1ab47d.js
@@ -0,0 +1,1 @@
+import{a as e,b as n,o as c}from"./entry.2f672c90.js";const o={};function r(t,a){return c(),n("div",null," villa index ")}const l=e(o,[["render",r]]);export{l as default};
